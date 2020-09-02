90f0c098b35e347a86475173e72e1a746a6d93df
alan-santos-helloworld2020 <70154815+alan-santos-helloworld2020@users.noreply.github.com>
2020-09-02 20:20:51 -0300

    Update router.js

--- router/router.js
+++ router/router.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var path = require('path');
 
 router.use(express.static(path.join(__dirname, "../", 'public')));
-router.use(express.static(path.join(__dirname, "../", 'views')));
+
 router.get('/', (req, res) => {
-    res.sendFile('index.html');
+    res.sendFile(path.join(__dirname, '../', 'views','index.html'));
 
 
 });
