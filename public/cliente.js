0384b76fc82ba7f4c62496cb08f83790a454f577
alan <alanprotesto@gmail.com>
2020-09-02 00:47:09 -0300

    basico de chat

---
+++ public/cliente.js
@@ -0,0 +1,120 @@
+var messenger = {
+
+
+    props: {
+
+        ms:{},
+        userself:{
+            type:String
+        }
+
+    },
+    data:()=>{
+     
+
+        return{
+            user:{
+                data:"col s12 center green lighten-4",
+                hora:"col s12 center green lighten-5",
+                nome:"col s12 left purple-text",
+                msg:"col s12 left grey lighten-5"
+
+        }
+    }
+
+    },
+    template: `
+    <div class="row">
+        <div class="col s10 push-s1">
+
+            <div :class="userself">
+                <div>Data: {{ms.data}}</div>
+            </div>
+
+            <div :class="user.hora">
+                <div>Hora: {{ms.hora}}</div>
+            </div>
+
+            <div :class="user.nome">
+                <div>{{ms.nome}}</div>
+            </div>
+
+            <div :class="user.msg">
+                <div>{{ms.mensagem}}</div>
+            </div>
+
+        </div>
+    </div>
+ 
+    `
+
+};
+
+//**************************************************************** */
+
+let root = new Vue({
+
+    el: "#root",
+  
+    data: {
+        userself:"",
+        socket: io(),
+        ver: false,
+        msg: {
+            id: null,
+            data: null,
+            hora: null,
+            nome: null,
+            mensagem: null,
+            style:""
+        },
+        pac_msg: []
+    },
+    created() {
+
+        do {
+
+            this.msg.nome = prompt("Informe seu nome");
+            this.ver = true;
+
+        } while (!this.msg.nome);
+
+    },
+    methods: {
+        enviar() {
+
+            this.socket.emit('online', this.msg);
+            this.msg.mensagem = null;
+
+
+        },
+        resposta() {
+
+            this.socket.on('online', (msg) => {
+                if (msg.nome == this.msg.nome) {
+                    msg.style="center green lighten-4";
+                    msg.nome = "Você";
+                    
+                                        
+                }else{
+                    msg.style = "center blue lighten-4";
+
+                }
+
+                this.pac_msg.push(msg);
+                return false;
+            });
+        }
+
+    },
+     
+     components:{"my-msg":messenger}
+    
+
+}).resposta();
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var path = require('path');
+var moment = require('moment');
+var http = require('http');
+var socketio = require('socket.io');
+var porta = process.env.PORT || 3000;
+
+
+
+var app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+
+
+});
+
+io.on('connection', (socket) => {
+
+    socket.on('connection', (msg) => {
+        msg.mensagem = "Conectado";
+        io.emit('online', msg);
+        console.log('você está conectado');
+    });
+
+    socket.on('online', (msg) => {
+        msg.id = socket.id;
+        msg.data = moment().format('DD/MM/YYYY');
+        msg.hora = moment().format('hh:mm:ss');
+        io.emit('online', msg);
+        console.log(msg);
+    });
+
+});
+
+server.listen(porta, (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(`http://localhost:${porta}`);
+
+})
